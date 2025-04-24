322820c49a0eaecb5113624dc8c79a1ecf2a109f
Mirco <mirco.cotugno@gmail.com>
2025-04-24 10:28:07 +0200

    add space between acton elements

--- components/action.tsx
+++ components/action.tsx
@@ -46,7 +46,7 @@ export default function Action({ blok, parent, theme, size }: ActionComponent) {
           color={theme || blok.color || 'default'}
           size={size}
           href={link}
-          className='col-auto font-bold min-w-fit cursor-pointer'
+          className='col-auto font-bold min-w-fit cursor-pointer gap-2'
           {...storyblokEditable(blok)}
         >
           {Label}
@@ -57,7 +57,7 @@ export default function Action({ blok, parent, theme, size }: ActionComponent) {
   return (
     <Container>
       <Link
-        className='col-auto font-medium min-w-fit cursor-pointer'
+        className='col-auto font-medium min-w-fit cursor-pointer gap-2'
         href={link}
         target={blok.link.target}
         color={theme || blok.color || 'foreground'}
